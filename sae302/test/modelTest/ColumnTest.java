3d8a5334e3fe4f666a4603df2b50ef1cbfdb8265
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2022-11-02 19:09:39 +0100

    ColumnTest add test_set_normalizer

--- sae302/test/modelTest/ColumnTest.java
+++ sae302/test/modelTest/ColumnTest.java
@@ -1,6 +1,6 @@
 package modelTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 
@@ -8,9 +8,8 @@ import org.junit.Test;
 
 import model.Column;
 import model.DataSet;
-import model.EnumValueNormalizer;
 import model.NumberValueNormalizer;
-import model.StringValueNormalizer;
+
 
 public class ColumnTest {
 	
@@ -23,9 +22,8 @@ public class ColumnTest {
     @Test
     public void test_set_normalizer() {
         Column column1 = new Column("Column1", new DataSet(), new ArrayList<>(), null);
-        assertEquals("NN", new NumberValueNormalizer("NUMBER_NORMALIZER").getValueNormalizer());
-        assertEquals("EN", new EnumValueNormalizer("ENUM_NORMALIZER").getValueNormalizer());
-        assertEquals("SN", new StringValueNormalizer("STRING_NORMALIZER").getValueNormalizer());
+        column1.setNormalizer(new NumberValueNormalizer("NUMBER_NORMALIZER"));
+        assertEquals("NN", column1.getNormalizer().getValueNormalizer());
     }
 
 
