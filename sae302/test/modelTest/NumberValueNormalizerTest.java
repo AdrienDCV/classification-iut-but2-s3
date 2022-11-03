19b807a430e9f8d42462eee0138bce66785a59f0
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2022-11-03 16:58:08 +0100

    Reecriture de NumberValueNormalizer, modifications apportées à test_number_value_normalizer_constrcutor

--- sae302/test/modelTest/NumberValueNormalizerTest.java
+++ sae302/test/modelTest/NumberValueNormalizerTest.java
@@ -5,14 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 
+import model.Column;
 import model.NumberValueNormalizer;
 
 public class NumberValueNormalizerTest {
     
     @Test
     public void test_number_value_normalizer_constructor() {
+        NumberColumn nColumn = new NumberColumn();
         assertEquals("NN", new NumberValueNormalizer().getValueNormalizer());
-    
+
+        assertNotEquals("BN", new NumberValueNormalizer().getValueNormalizer());
+        assertNotEquals("", new NumberValueNormalizer().getValueNormalizer());
+        assertNotEquals(null, new NumberValueNormalizer().getValueNormalizer());
     }
 
 }
