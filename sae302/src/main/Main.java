138bb303d5e53f55d2acab766d18855b56a6a0fe
ylann gronnier <ylann.gronnier.etu@univ-lille.fr>
2022-11-09 11:09:19 +0100

    debut main

--- sae302/src/main/Main.java
+++ sae302/src/main/Main.java
@@ -1,9 +1,34 @@
 package main;
 
-public class Main {
+import javafx.application.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    Button confirmer=new Button("confirmer");
+    Button parcourir=new Button("parcourir");
+    Button sauvegarde=new Button("sauvegarder");
+    ComboBox premiere_var=new ComboBox();
+    ComboBox deuxieme_var=new ComboBox();
+    TextField entrer_k=new TextField();
+    FileChooser fichier_csv=new FileChooser();
+    
+    
+    
+    public void start(Stage stage) throws Exception {
+    	
+    	VBox vbox=new Vbox();
+    	vbox.getChildren().addAll(confirmer,parcourir,sauvegarder,premiere_var,deuxieme_var,champs_k);
+    	Scene scene=new Scene(vbox,900,900);
+    	stage.setTitle("test");
+    	stage.setScene(scene);
+    	stage.show();
+    	
+    }
     
     public static void main(String[] args) {
-        System.out.println("tout va bien !");
-    }
+		launch(args);
+	}
 
 }
