d26e7d58ace1a82c3a5272f13bb8fdc12bd6e743
Younes Ben Dhiab <younes.bendhiab.etu@univ-lille.fr>
2022-11-10 17:27:12 +0100

    ScatterChartObject Kinda Done + saveScatterChart as Image

--- sae302/src/main/ScatterChartObject.java
+++ sae302/src/main/ScatterChartObject.java
@@ -1,22 +1,28 @@
 package main;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
-import intefarces.IColumn;
+import javax.imageio.ImageIO;
+
 import intefarces.IPoint;
-import javafx.scene.Group;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
 import model.Column;
+
 
 public class ScatterChartObject {
 	ScatterChart<Double, Double> scatterChart;
-
 	Column xCol; 
 	Column yCol;
 
-	public ScatterChartObject(List<IColumn> firstList, List<IColumn> secondList) {
+	
+//	Column xCol, Column yCol
+	public ScatterChartObject() {
 		NumberAxis x = new NumberAxis();
 		x.setLabel("Axe des X");
 		NumberAxis y = new NumberAxis();
@@ -28,7 +34,6 @@ public class ScatterChartObject {
 		XYChart.Series<Double, Double> series2 = new XYChart.Series<Double, Double>();
 		series1.setName("x");
 		series2.setName("y");
-		
 		for (int i=0; i < this.xCol.getPointsList().size(); i++) {
 			 IPoint xCurrentPoint = xCol.getPointsList().get(i);
 			 IPoint yCurrentPoint = yCol.getPointsList().get(i);
@@ -51,6 +56,24 @@ public class ScatterChartObject {
 
 	public void setScatterChart(ScatterChart<Double, Double> scatterChart) {
 		this.scatterChart = scatterChart;
+	}
+	
+	public void saveScatterChart() {
+		String path;
+		if(new File(System.getProperty("user.home") + "/Bureau").exists()) {
+			path = System.getProperty("user.home") + "/Bureau/image.png";
+		} else {
+			path = System.getProperty("user.home") + "/Desktop/image.png";
+		}
+		File file = new File(path);
+		WritableImage image = this.scatterChart.snapshot(new SnapshotParameters(), null);
+		try {
+		  ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			e.getStackTrace();
+		}
+		
 	}
 	
 }
