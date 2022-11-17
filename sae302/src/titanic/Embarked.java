c0180049ed1b0e89c3d5ba137f5849f38c09fbac
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2022-11-17 14:42:53 +0100

    modifs Embarked et Titanic

--- sae302/src/titanic/Embarked.java
+++ sae302/src/titanic/Embarked.java
@@ -1,5 +1,22 @@
 package titanic;
 
 public enum Embarked {
-	S, C, Q;
+	S("S",1), C("C",2), Q("Q",3);
+
+	private String label;
+	private int idx;
+
+	Embarked(String label, int idx) {
+		this.label = label;
+		this.idx = idx;
+	}
+
+	public int getIdx() {
+		return this.idx;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
 }
