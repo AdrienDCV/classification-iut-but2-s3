344cd146f6df97444b8bc49800773e682d20d86a
ylann gronnier <ylann.gronnier.etu@univ-lille.fr>
2022-11-24 09:32:42 +0100

    test distance (fin)

--- sae302/test/modelTest/DistanceTest.java
+++ sae302/test/modelTest/DistanceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import intefarces.IPoint;
 import model.Criteria;
 import model.DistanceEuclidienne;
+import model.DistanceManhattan;
 import model.NumberColumn;
 import pokemon.Pokemon;
 import pokemon.PokemonDataSet;
@@ -16,7 +17,7 @@ import pokemon.PokemonDataSet;
 public class DistanceTest {
 
 	@Test
-	public void distaceEuclidienneTest() {
+	public void distanceEuclidienneTest() {
 		
 		PokemonDataSet dataset=new PokemonDataSet("test");
 		dataset.loadFromFile("./res/pokemon_train.csv");
@@ -24,7 +25,18 @@ public class DistanceTest {
 		Pokemon poke2=new Pokemon("test2",1000,30720,3, 0, 0, 0, 0, 0, "", "", 0, true);
 		
 		DistanceEuclidienne distance=new DistanceEuclidienne();
-		assertEquals(42,distance.calculDistance(poke1, poke2,dataset.getColumnsList().get(1),dataset.getColumnsList().get(2) ));
+		assertEquals(0.16989449312079544,distance.calculDistance(poke1, poke2,dataset.getColumnsList().get(1),dataset.getColumnsList().get(2) ),0.001);
 	
+	}
+	
+	@Test
+	public void distanceManhatanTest() {
+		PokemonDataSet dataset=new PokemonDataSet("test");
+		dataset.loadFromFile("./res/pokemon_train.csv");
+		Pokemon poke1=new Pokemon("test1",1000,30720,45, 0, 0, 0, 0, 0, "", "", 0, true);
+		Pokemon poke2=new Pokemon("test2",1000,30720,3, 0, 0, 0, 0, 0, "", "", 0, true);
+		
+		DistanceManhattan distance=new DistanceManhattan();
+		assertEquals(0.2063725490196078,distance.calculDistance(poke1, poke2,dataset.getColumnsList().get(1),dataset.getColumnsList().get(2) ),0.001);
 	}
 }
