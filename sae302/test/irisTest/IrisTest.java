26521647855c61def64bdc5fc77b46445315b11c
ylann gronnier <ylann.gronnier.etu@univ-lille.fr>
2022-11-29 10:12:57 +0100

    tests

---
+++ sae302/test/irisTest/IrisTest.java
@@ -0,0 +1,54 @@
+package irisTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import iris.Iris;
+import model.Column;
+import model.NumberColumn;
+
+public class IrisTest {
+	Iris iris1;
+	
+	@BeforeEach
+	public void setup() {
+		iris1=new Iris(0,0,0,0,"c");
+	}
+	
+	@Test
+	public void toStringTest() {
+		assertEquals("Iris [sepalLength=0.0, sepalWidth=0.0, petalLength=0.0, petalWidth=0.0, variety=c]",iris1.toString());
+	}
+	
+	@Test
+	public void getValueSepalLengthTest() {
+		NumberColumn col=new NumberColumn("sepal.length", null, null);
+		assertEquals(0.0,iris1.getValue(col));
+	}
+	
+	@Test
+	public void getValueSepalWidth() {
+		NumberColumn col=new NumberColumn("sepal.width", null, null);
+		assertEquals(0.0,iris1.getValue(col));
+	}
+	
+	@Test
+	public void getValuePetalLengthTest() {
+		NumberColumn col=new NumberColumn("petal.length", null, null);
+		assertEquals(0.0,iris1.getValue(col));
+	}
+	
+	@Test
+	public void getValuePetalWidth() {
+		NumberColumn col=new NumberColumn("petal.width", null, null);
+		assertEquals(0.0,iris1.getValue(col));
+	}
+	
+	@Test
+	public void getValueVarietyTest() {
+		NumberColumn col=new NumberColumn("variety", null, null);
+		assertEquals("c",iris1.getValue(col));
+	}
+}
